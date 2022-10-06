3f8e7cd4fe724f458ecf71abc07b545b14203421
Dimitri Perriet <dimitri.perriet@outlook.frls>
2022-10-06 16:17:24 +0200

    ADD: PlatformFactory

--- classes/PlatformFactory.class.php
+++ classes/PlatformFactory.class.php
@@ -1,5 +1,37 @@
 <?php
 
-class PlatformFactory{
+abstract class PlatformFactory{
+
+    public abstract function createFactory();
+
+}
+
+class BinancePlatformFactory extends PlatformFactory{
+
+    public function createFactory(){
+        $trader = new Trader();
+        $analyser = new Analyser();
+        return new BinancePlatform($trader, $analyser);
+    }
+
+}
+
+class BinancePlatform implements Platform {
+
+    private Trader $_trd;
+    private Analyser $_anl;
+
+    public function __construct($trd, $anl){
+        $this->_trd = $trd;
+        $this->_anl = $anl;
+    }
+
+    public function getTrader(){
+        return $this->_trd;
+    }
+
+    public function getAnalyser(){
+        return $this->_anl;
+    }
 
 }
