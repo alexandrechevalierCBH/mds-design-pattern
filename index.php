69c776fe48204a6d445d0d41dfa5dbf4ca0410f2
Alexandre Chevalier <alexandre.chevalier@gmx.fr>
2022-10-06 15:20:02 +0200

    MaJ Trader Banker

--- index.php
+++ index.php
@@ -3,7 +3,11 @@
 require_once 'classes/DBConnector.class.php';
 //require wallet class
 require_once 'classes/Wallet.class.php';
+require_once 'classes/Trader.class.php';
+
 
 $db = DBConnector::getInstance()->getConnection();
 $balance = Wallet::getBalance();
-echo $balance;
+
+$testbuy = new Trader();
+$testbuy->buy(5001);
