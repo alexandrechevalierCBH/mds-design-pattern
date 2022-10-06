431d524cf4ae00daa73a72124d46200dd7c0ec48
Dimitri Perriet <dimitri.perriet@outlook.frls>
2022-10-06 14:48:31 +0200

    NEW : DataRetriever & Proxy

---
+++ classes/proxyDataRetriver.class.php
@@ -0,0 +1,22 @@
+<?php
+Class ProxyDataRetriever{
+    private $dataRetriever;
+    private $data = array();
+    private int $getLastUpdate;
+
+    public function __construct(DataRetriever $dataRetriever){
+        $this->getLastUpdate = 0;
+        $this->dataRetriever = $dataRetriever;
+    }
+
+    public function getData(){
+        if(time() - $this->dataRetriever->getLastUpdate > 60){
+            $this->data = $this->dataRetriever->getData();
+            $this->getLastUpdate = time();
+        }
+        return $this->data;
+    }
+    public function getPairPrice($pair){
+        return $this->dataRetriever->getPairPrice($pair);
+    }
+}
