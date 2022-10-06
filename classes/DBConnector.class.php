fdbed3d6d48229930156df4cbb8d23e186f69c5a
Alexandre Chevalier <alexandre.chevalier@gmx.fr>
2022-10-06 11:04:37 +0200

    Added core classes

--- classes/DBConnector.class.php
+++ classes/DBConnector.class.php
@@ -1,1 +1,48 @@
 <?php
+class DBConnector
+{
+    private static $instance = null;
+    private $conn;
+    private $host = 'localhost';
+    private $user = 'root';
+    private $pass = '';
+    private $name = 'test';
+
+    /**
+     * The function is private, so it can only be called from within the class. It creates a new
+     * database connection and returns it
+     */
+    private function __construct()
+    {
+        $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->name);
+        if (mysqli_connect_error()) {
+            trigger_error(
+                "Failed to conencto to MySQL: " . mysql_connect_error(),
+                E_USER_ERROR
+            );
+        }
+    }
+
+    /**
+     * > If there is no instance of the class, create one. If there is, return the existing one
+     * 
+     * @return The instance of the DBConnector class.
+     */
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new DBConnector();
+        }
+        return self::$instance;
+    }
+
+    /**
+     * It returns the connection object
+     * 
+     * @return The connection to the database.
+     */
+    public function getConnection()
+    {
+        return $this->conn;
+    }
+}
