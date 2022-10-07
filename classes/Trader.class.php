8bb64f57719426b819be3aa68249547ec5b70e38
Marcelin <marcelinbertrandmontembault@gmail.com>
2022-10-07 14:23:36 +0200

    NEW TraderFactory

--- classes/Trader.class.php
+++ classes/Trader.class.php
@@ -4,17 +4,10 @@ class Trader
 {
     public function buy($amount, $qty, $unit_price)
     {
-        $bank = Banker::canBuy($amount, $qty, $unit_price);
-        if ($bank) {
-            echo 'I buy crypto' . '<br/>';
-        } else {
-            echo 'Not enouth money';
-        }
     }
 
     public function sell($amount, $qty, $unit_price)
     {
-        $bank = Banker::getOseille($amount, $qty, $unit_price);
-        echo 'Je vend de la crypto';
+    
     }
 }
